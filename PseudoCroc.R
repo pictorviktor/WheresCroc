034024e5a6eb98ecb97d87b2a6ba6da44bd4e88e
Viktor Evestam <viktor.e.evestam@gmail.com>
2023-10-01 12:58:36 +0200

    Mer update

--- PseudoCroc.R
+++ PseudoCroc.R
@@ -1,9 +1,14 @@
 # Pseudo Croc
 
-## Markov
+## Markov-function
 ## Inparameter = salinity, phosphate and nitrogen levels för alla positioner
 ## För varje drag beräkna Markov utifrån salinity, phosphate and nitrogen levels
-
-
+## - på samtliga positioner
 
 ## Utparameter = Sannolikhet att Croc är på en given plats. Lista, matris eller variabel?
+
+## Position-function
+## Inparameter = Markovfunction
+## Utifrån inparameter, välj nästa position
+
+## Logik för att utföra ett steg (Givet av uppgiften)
